21682d6a157997885c35dc9c75a4804a3907b0bf
lekonai <lekonai@users.noreply.github.com>
2025-07-27 18:50:32 +0100

    quick commit

--- prereqs.js
+++ prereqs.js
@@ -1,3 +1,5 @@
 // this will contain essential html code such as the navigation bar
-
-document.log("<h1>hell yeah</h1>");
+// just need to figure how it works though lol
+const heading = document.createElement("h1");
+const text = document.createTextNode("HELLO!");
+heading.appendChild(text);
